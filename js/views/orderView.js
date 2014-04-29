bade20894bc8acba36435e2057d922dbf41501b2
Leighton Kaina <leighton.kaina@gmail.com>
2014-04-28 23:24:58 -0700

    Add email to table

--- js/views/orderView.js
+++ js/views/orderView.js
@@ -5,6 +5,7 @@ var OrderView = Parse.View.extend({
     '<td><%= date %>'+
     '<td><a download="<%= user %>_<%= salt %>_front.jpg" href="<%= frontImg %>"><img src=<%= frontImg %> ></img></a>'+
     '<td><a download="<%= user %>_<%= salt %>_back.jpg" href="<%= backImg %>"><img src=<%= backImg %> ></img></a>'+
+    '<td><a href="mailto:<%= email %>" class="eText"><%= email %></a></td>'+
     '<td><input type="checkbox" class="toggle" <%= checked %> >'
   ),
 
@@ -22,7 +23,10 @@ var OrderView = Parse.View.extend({
   },
 
   render: function() {
+    var self = this;
     var checked = '';
+    var email = '';
+
     if (this.model.get('completed')){
       checked = 'checked';
       this.$el.addClass('complete');
@@ -31,6 +35,16 @@ var OrderView = Parse.View.extend({
       // Can't do in model because of fetch process in collection
       this.model.initComplete();
     }
+    var query = new Parse.Query(Parse.User);
+    query.equalTo('username', this.model.get('user').id);
+    query.first({
+      success: function(user){
+        email = user.get('email');
+      },
+      error: function(err){
+        console.log("Error retrieving user's email");
+      }
+    });
     return this.$el.html(this.template({
       date: this.model.createdAt.toLocaleString(),
       salt: this.model.createdAt.getTime(),
@@ -38,6 +52,7 @@ var OrderView = Parse.View.extend({
       frontImg: this.model.get('front_image').url(),
       backImg: this.model.get('back_image').url(),
       fullImg: this.model.get('full_image').url(),
+      email: this.model.get('email'),
       checked: checked
     }));
   }
